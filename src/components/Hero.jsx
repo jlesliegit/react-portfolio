9eb231615ca1d67b398a117c99ad2dbf04a12b66
Jack Leslie <jackandrewleslie@gmail.com>
2025-03-14 15:55:11 +0000

    Made responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ import NavigationLinks from "../atoms/NavigationLinks.jsx";
 
 export default function Hero() {
     return(
-        <div className={"grid grid-cols-4"}>
-            <div className={"flex flex-col gap-3 col-start-2 items-start pt-52"}>
+        <div className={"grid grid-cols-4 max-sm:grid-cols-1"}>
+            <div className={"flex flex-col gap-4 col-start-2 items-start pt-52 max-sm:col-start-1 max-sm:text-left max-sm:ml-10"}>
                 <Title />
                 <Tagline />
                 <SocialLinks />
