df87e46fa28972bf7fd8aba91e7a37d3e0c3ab0d
Jack Leslie <jackandrewleslie@gmail.com>
2025-04-10 12:09:01 +0100

    Restyled all projects page

--- src/atoms/Technologies.jsx
+++ src/atoms/Technologies.jsx
@@ -2,7 +2,7 @@ export default function Technologies({ name }) {
 
     return (
         <div className={'flex text-center gap-1'}>
-        <p className={'inline-block whitespace-nowrap w-fit px-3 py-1 bg-gray-100 text-sm text-gray-800 rounded-lg shadow-sm hover:bg-blue-100 transition'}>{name}</p>
+        <p className={'inline-block whitespace-nowrap w-fit px-3 py-1 bg-gray-100 text-sm text-gray-800 rounded-lg shadow-sm'}>{name}</p>
         </div>
     );
 }
