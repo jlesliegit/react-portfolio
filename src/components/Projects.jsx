945f434eec92cff42d71dd5ed9eb803cd34a10f7
Jack Leslie <jackandrewleslie@gmail.com>
2026-01-08 09:47:03 +0000

    added additional text explaining why current project live links no longer work

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,35 @@
 import SingleProjectBox from "../atoms/SingleProjectBox.jsx";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Nav from "./Nav.jsx";
 
 export default function Projects() {
-    const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-        fetch("/projects.json")
-            .then((response) => response.json())
-            .then((data) => setProjects(data.projects))
-    }, []);
+  useEffect(() => {
+    fetch("/projects.json")
+      .then((response) => response.json())
+      .then((data) => setProjects(data.projects));
+  }, []);
 
-    return (
-        <>
-            <Nav link={'/'} destination={'Home'} />
-        <div className="grid grid-cols-1 sm:grid-cols-2 sm:px-10 sm:items-start">
-            <div className="flex flex-col text-left gap-4 px-4 mb-10 sm:max-w-screen-md sm:w-full sm:ml-20 sm:pl-10 pt-52 max-sm:px-8">
-                {projects.map((project) => (
-                    <SingleProjectBox
-                        key={project.id}
-                        title={project.title}
-                        snippet={project.snippet}
-                        id={project.id}
-                    />
-                ))}
-            </div>
+  return (
+    <>
+      <Nav link={"/"} destination={"Home"} />
+      <div className="grid grid-cols-1 sm:grid-cols-2 sm:px-10 sm:items-start">
+        <div className="flex flex-col text-left gap-4 px-4 mb-10 sm:max-w-screen-md sm:w-full sm:ml-20 sm:pl-10 pt-52 max-sm:px-8">
+          {projects.map((project) => (
+            <SingleProjectBox
+              key={project.id}
+              title={project.title}
+              snippet={project.snippet}
+              id={project.id}
+            />
+          ))}
+          <p>
+            Bootcamp projects built using shared infrastructure that has since
+            been decommissioned.
+          </p>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
